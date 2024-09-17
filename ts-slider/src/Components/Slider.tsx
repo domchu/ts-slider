cbd015953aeb4c93883f68f04908930d53aea96b
Mr.Dominic <ochu.dominic.onah@gmail.com>
2024-09-17 15:12:57 +0100

    Chore:functionality for the next btn handler

--- ts-slider/src/Components/Slider.tsx
+++ ts-slider/src/Components/Slider.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 type SliderProps = {
   slides: {
     id: number;
@@ -6,13 +8,35 @@ type SliderProps = {
     description: string;
     link: string;
     button: string;
-  };
+  }[];
 };
 
 const Slider = ({ slides }: SliderProps) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleNextBtn = () => {
+    const nextIndex = (currentIndex + 1) % slides.length;
+    setCurrentIndex(nextIndex);
+  };
+  const handlePrevBtn = () => {};
+
   return (
     <>
-      <div className="slider-container"></div>
+      <div className="slider_container">
+        <div className="slider_image">
+          <img
+            src={slides[currentIndex].image}
+            alt="Slider Image"
+            className="slider_image"
+          />
+        </div>
+        <div className="slider-content">
+          <h2>{slides[currentIndex].title}</h2>
+          <p>{slides[currentIndex].description}</p>
+        </div>
+        <button onClick={handlePrevBtn}>Prev</button>
+        <button onClick={handleNextBtn}>Next</button>
+      </div>
     </>
   );
 };
